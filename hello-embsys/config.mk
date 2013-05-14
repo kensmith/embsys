33f9aa4c2e5f2e93ba125bc8fe0852f48d4c2780
Ken Smith <kgsmith@gmail.com>
2013-05-13 22:14:54 -0700

    fixed bug that caused the demo code during the presentation not to be optimized :(

--- hello-embsys/config.mk
+++ hello-embsys/config.mk
@@ -1,8 +1,8 @@
 debug := -g
-init.cpp-opt := -Os
-led.cpp-opt := -Os
-main.cpp-opt := -Os
-print.cpp-opt := -Os
+src/init.cpp-opt := -Os
+src/led.cpp-opt := -Os
+src/main.cpp-opt := -Os
+src/print.cpp-opt := -Os
 toolchain := arm-none-eabi
 show :=
 MAKEFLAGS := j4
